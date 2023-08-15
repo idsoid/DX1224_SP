04bef181be062f281ca4cbf5be53c20262f92040
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-15 15:38:54 +0800

    Added exit door

---
+++ Unity_SP/Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject closeSprite;
+    [SerializeField] private GameObject openSprite;
+
+    private bool closed;
+
+    private void Start()
+    {
+        closed = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            ToggleClose(!closed);
+        }
+    }
+
+    public void ToggleClose(bool x)
+    {
+        closed = x;
+        if (x)
+        {
+            closeSprite.SetActive(true);
+            openSprite.SetActive(false);
+        }
+        else
+        {
+            closeSprite.SetActive(false);
+            openSprite.SetActive(true);
+        }
+    }
+}
