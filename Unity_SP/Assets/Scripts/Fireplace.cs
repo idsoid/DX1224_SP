f618b655b3ef0c4cd1f80b5348b67b6c3f42a4f1
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-15 14:26:11 +0800

    Added Fireplace script

---
+++ Unity_SP/Assets/Scripts/Fireplace.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour
+{
+    [SerializeField] private GameObject fire;
+    [SerializeField] private GameObject safeZone;
+    [SerializeField] private Animator fireplaceAnim;
+
+    private bool lit;
+    public float burnTime;
+
+    private void Start()
+    {
+        lit = true;
+        burnTime = 180f;
+    }
+
+    private void Update()
+    {
+        if (lit)
+        {
+            if (burnTime > 0f)
+            {
+                burnTime -= 1f * Time.deltaTime;
+            }
+            else if (burnTime < 0f)
+            {
+                burnTime = 0f;
+            }
+            else if (burnTime == 0f)
+            {
+                ToggleLit(false);
+            }
+        }
+    }
+
+    private void ToggleLit(bool x)
+    {
+        lit = x;
+        fire.SetActive(lit);
+        safeZone?.SetActive(lit);
+        if (!x)
+        {
+            fireplaceAnim.Play("fireplace_unlit");
+        }
+        else
+        {
+            fireplaceAnim.Play("fireplace_lit");
+        }
+    }
+
+    public void AddFuel()
+    {
+        burnTime += 60;
+        if (!lit)
+        {
+            ToggleLit(true);
+        }
+    }
+}
