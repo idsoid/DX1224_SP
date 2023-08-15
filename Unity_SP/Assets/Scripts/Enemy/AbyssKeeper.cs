0fc3b3f153f01bd6d8c6d35aa64bb45c16fbf327
idsoid <125327978+idsoid@users.noreply.github.com>
2023-08-15 12:59:58 +0800

    folders + enemy ai base

--- Unity_SP/Assets/Scripts/Enemy/AbyssKeeper.cs
+++ Unity_SP/Assets/Scripts/Enemy/AbyssKeeper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class BaseEnemy : MonoBehaviour
+public class AbyssKeeper : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
