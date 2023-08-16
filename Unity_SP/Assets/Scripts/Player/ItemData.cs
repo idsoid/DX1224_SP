d0d5065c23df8a583eab86a70d785674477e896e
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-16 10:41:52 +0800

    Added item data script

--- Unity_SP/Assets/Scripts/Player/ItemData.cs
+++ Unity_SP/Assets/Scripts/Player/ItemData.cs
@@ -2,17 +2,177 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int itemID;
+    [SerializeField] private string itemName;
+    [SerializeField] private Sprite itemSprite;
+    [SerializeField] private PlayerData playerData;
+
+    private bool equipped;
+
+    private void Start()
     {
-        
+        equipped = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+    <========== Key Items ==========>
+        1. Key1
+        2. Key2
+        3. Key3
+    <========== Weapons ==========>
+        20. Fireplace Poker +10 atk
+        21. Candle Stand +5 atk
+        22. Broken Table Leg +5 atk
+        23. Kitchen Knife +20 atk
+        24. Cast Iron Pan +10 atk
+    <========== Charms ==========>
+        30. Attack Charm +7 atk
+        31. Speed Charm +5 spd
+        32. Anti-hunger Charm 
+        33. Heat Charm 
+        34. Starvation Charm +7 atk if hunger < 50
+    <========== Consumables ==========>
+        50. Health Potion +20 hp
+        51. Food +20 hunger
+    */
+
+    public void UseItem()
     {
-        
+        if (itemID == 20) // Fireplace Poker
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 10);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -10);
+            }
+        }
+        else if (itemID == 21) // Candle Stand
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 5);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -5);
+            }
+        }
+        else if (itemID == 22) // Broken Table Leg
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 5);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -5);
+            }
+        }
+        else if (itemID == 23) // Kitchen Knife
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 20);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -20);
+            }
+        }
+        else if (itemID == 24) // Cast Iron Pan
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 10);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -10);
+            }
+        }
+        else if (itemID == 30) // Attack Charm
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("attack", 7);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("attack", -7);
+            }
+        }
+        else if (itemID == 31) // Speed Charm
+        {
+            if (!equipped)
+            {
+                equipped = true;
+                playerData.AlterValue("speed", 5);
+            }
+            else
+            {
+                equipped = false;
+                playerData.AlterValue("speed", -5);
+            }
+        }
+        else if (itemID == 32) // Anti-hunger Charm
+        {
+            if (!equipped)
+            {
+                equipped = true;
+            }
+            else
+            {
+                equipped = false;
+            }
+        }
+        else if (itemID == 32) // Heat Charm
+        {
+            if (!equipped)
+            {
+                equipped = true;
+            }
+            else
+            {
+                equipped = false;
+            }
+        }
+        else if (itemID == 32) // Starvation Charm
+        {
+            if (!equipped)
+            {
+                equipped = true;
+            }
+            else
+            {
+                equipped = false;
+            }
+        }
+        else if (itemID == 50) // Health Potion
+        {
+            playerData.AlterValue("health", 20);
+        }
+        else if (itemID == 51) // Food
+        {
+            playerData.AlterValue("hunger", 20);
+        }
+
     }
 }
