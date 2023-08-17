5de5395db7a2d5cc5ccd5b85c63c74424dc8dbac
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-17 14:19:09 +0800

    Added sprites for 3 locks

--- Unity_SP/Assets/Scripts/World/ExitDoor.cs
+++ Unity_SP/Assets/Scripts/World/ExitDoor.cs
@@ -6,12 +6,15 @@ public class ExitDoor : MonoBehaviour
 {
     [SerializeField] private GameObject closeSprite;
     [SerializeField] private GameObject openSprite;
+    [SerializeField] private GameObject lockPanel;
 
     private bool closed;
+    private bool playerInteract;
 
     private void Start()
     {
         closed = true;
+        playerInteract = false;
     }
 
     public void ToggleClose(bool x)
@@ -26,6 +29,30 @@ public class ExitDoor : MonoBehaviour
         {
             closeSprite.SetActive(false);
             openSprite.SetActive(true);
+        }
+
+        if (playerInteract)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                lockPanel.SetActive(true);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInteract = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInteract = false;
         }
     }
 }
