f54b7580879ec59f565af2ccfb1fca1d21f6a7a8
asher505 <222349U@mymail.nyp.edu.sg>
2023-08-17 13:31:54 +0800

    hud finish

--- Unity_SP/Assets/Scripts/Player/UpdateHUD.cs
+++ Unity_SP/Assets/Scripts/Player/UpdateHUD.cs
@@ -10,24 +10,87 @@ public class UpdateHUD : MonoBehaviour
     public Image healthBar;
     public Image staminaBar;
     public Image hungerBar;
-    public GameObject temperatureIndicator;
+    public GameObject frost1;
+    public GameObject frost2;
+    public GameObject frost3;
+    public GameObject frost4;
 
-    public playerData pData;
+    public PlayerData playerData;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float lerpSpeed;
+
+    float quintTemp;
+
 
     // Update is called once per frame
     void Update()
     {
-        //temperatureIndicator.Find("frost1").gameObject.SetActive(true);
+        quintTemp = playerData.GetValue("maxTemperature") / 4;
+
+        if (playerData.GetValue("temperature") < quintTemp)
+        {
+            if (frost4.activeSelf)
+            {
+                //frost4.SetActive(false);
+            }
+            else if (!frost4.activeSelf)
+            {
+                frost4.SetActive(true);
+                frost3.SetActive(false);
+            }
+        }
+        else if (playerData.GetValue("temperature") < quintTemp * 2)
+        {
+            if (frost3.activeSelf)
+            {
+                //frost3.SetActive(false);
+            }
+            else if (!frost3.activeSelf)
+            {
+                frost3.SetActive(true);
+                frost2.SetActive(false);
+            }
+        }
+        else if (playerData.GetValue("temperature") < quintTemp * 3)
+        {
+            if (frost2.activeSelf)
+            {
+                //frost2.SetActive(false);
+            }
+            else if (!frost2.activeSelf)
+            {
+                frost2.SetActive(true);
+                frost1.SetActive(false);
+            }
+        }
+        else if (playerData.GetValue("temperature") < quintTemp * 4)
+        {
+            if (frost1.activeSelf)
+            {
+                //frost1.SetActive(false);
+            }
+            else if (!frost1.activeSelf)
+            {
+                frost1.SetActive(true);
+            }
+        }
+        else
+        {
+            frost1.SetActive(false);
+            frost2.SetActive(false);
+            frost3.SetActive(false);
+            frost4.SetActive(false);
+        }
+
+        lerpSpeed = 3f * Time.deltaTime;
+
+        setBars();
     }
 
     private void setBars()
     {
-        //healthBar.fillAmount = pData.health / 
+        healthBar.fillAmount = playerData.GetValue("health") / playerData.GetValue("maxHealth");
+        staminaBar.fillAmount = Mathf.Lerp(staminaBar.fillAmount, playerData.GetValue("stamina") / playerData.GetValue("maxStamina"), lerpSpeed);
+        hungerBar.fillAmount = Mathf.Lerp(hungerBar.fillAmount, playerData.GetValue("hunger") / playerData.GetValue("maxHunger"), lerpSpeed);
     }
 }
