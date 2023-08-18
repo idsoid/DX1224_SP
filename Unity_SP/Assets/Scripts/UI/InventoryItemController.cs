05ee3d83cfde214bbc91a1a85fbed37aff410816
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-18 14:35:00 +0800

    Inventory now shows item count

--- Unity_SP/Assets/Scripts/UI/InventoryItemController.cs
+++ Unity_SP/Assets/Scripts/UI/InventoryItemController.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventoryItemController : MonoBehaviour
 {
     [SerializeField] private ItemData item;
     [SerializeField] private GameObject equippedIcon;
+    [SerializeField] private GameObject itemCountDisplay;
 
     public Button RemoveButton;
+
+    private TMP_Text itemCountText;
+
+    private void Start()
+    {
+        itemCountText = itemCountDisplay.GetComponent<TMP_Text>();
+    }
 
     private void Update()
     {
         if (item.GetEquipped() != equippedIcon.activeSelf)
         {
             equippedIcon.SetActive(item.GetEquipped());
+        }
+        if (item.GetCount() >= 2)
+        {
+            itemCountDisplay.SetActive(true);
+            itemCountText.text = "" + item.GetCount();
+        }
+        else
+        {
+            itemCountDisplay.SetActive(false);
         }
     }
 
