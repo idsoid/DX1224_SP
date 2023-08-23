91f3b79ab8c219e340eed22c00703491da2bea5c
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2023-08-23 11:54:19 +0800

    Finished keeper bullet hell

--- Unity_SP/Assets/Scripts/Bullet Hell/DelayedBullet.cs
+++ Unity_SP/Assets/Scripts/Bullet Hell/DelayedBullet.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class DelayedBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject target;
+    [SerializeField] private float timeToActivate;
+    [SerializeField] private float delayAfterActivate;
+
+    private Rigidbody2D rb;
+
+    private bool activate;
+    private Vector2 direction;
+    private float delay;
+    private bool shoot;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        activate = false;
+        delay = delayAfterActivate;
+        shoot = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (timeToActivate > 0)
+            timeToActivate -= Time.deltaTime;
+        else if (timeToActivate < 0)
+        {
+            direction = (target.transform.position - transform.position).normalized;
+            activate = true;
+            timeToActivate = 0;
+        }
+
+        if (activate)
+            Activated();
+    }
+
+    private void Activated()
+    {
+        if (delay > 0)
+            delay -= Time.deltaTime;
+        else if (delay < 0)
+        {
+            transform.parent = null;
+            shoot = true;
+            delay = 0;
+        }
+
+        if (shoot)
+        {
+            rb.velocity = Vector3.zero;
+            rb.velocity = direction * 150f;
+        }
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void SetTimeToActivate(float newTimeToActivate)
+    {
+        timeToActivate = newTimeToActivate;
     }
 }
